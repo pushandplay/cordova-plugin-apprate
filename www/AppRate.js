0eaea663e91aa10a88caf3b7c7b666a31b6fd022
gitare <git.are.dev@gmail.com>
2015-02-11 00:58:42 +0100

    JSON.parse() fix causing an 'Uncaught illegal access' on Android 2.3.4

--- www/AppRate.js
+++ www/AppRate.js
@@ -225,3 +225,13 @@ AppRate = (function() {
 AppRate.init();
 
 module.exports = AppRate;
+
+JSON.originalParse = JSON.parse;
+JSON.parse = function (text) {
+  if (text) {
+    return JSON.originalParse(text);
+  } else {
+    // no longer crashing on null value but just returning null
+    return null;
+  }
+}
