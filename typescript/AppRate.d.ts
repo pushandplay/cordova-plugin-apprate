5daa75423d9243050dbe1dc1dba7cf5b4a9e1d73
bohtm4711 <mikeboht@gmail.com>
2016-03-06 09:36:14 -0500

    Changing all variable and return types to lower case
    For definition files, capitalizing the case will still work, but it is
    standard in TypeScript to use lower case variable and return types.

--- typescript/AppRate.d.ts
+++ typescript/AppRate.d.ts
@@ -23,46 +23,46 @@ declare class AppRate {
 	static preferences:AppRatePreferences;
 	
 	static init():AppRate;
-	static promptForRating(immediately?:Boolean):AppRate;
+	static promptForRating(immediately?:boolean):AppRate;
 	static navigateToAppStore():AppRate;
 }
 
 declare class AppRatePreferences {
-	useLanguage:String;
-	displayAppName:String;
-	promptAgainForEachNewVersion:Boolean;
-	usesUntilPrompt:Number;
-	openStoreInApp:Boolean;
-	useCustomRateDialog:Boolean;
+	useLanguage:string;
+	displayAppName:string;
+	promptAgainForEachNewVersion:boolean;
+	usesUntilPrompt:number;
+	openStoreInApp:boolean;
+	useCustomRateDialog:boolean;
 	callbacks:CallbackPreferences;
 	storeAppURL:StoreAppURLPreferences;
 	customLocale:CustomLocale;
 }
 
 declare class StoreAppURLPreferences {
-	ios:String;
-	android:String;
-	blackberry:String;
-	windows8:String;
+	ios:string;
+	android:string;
+	blackberry:string;
+	windows8:string;
 }
 
 declare class CallbackPreferences {
-	onButtonClicked:(buttonIndex:Number) => void;
-	onRateDialogShow:(rateCallback:(buttonIndex:Number) => void) => void;
+	onButtonClicked:(buttonIndex:number) => void;
+	onRateDialogShow:(rateCallback:(buttonIndex:number) => void) => void;
 }
 
 declare class CustomLocale {
-	title:String;
-	message:String;
-	cancelButtonLabel:String;
-	laterButtonLabel:String;
-	rateButtonLabel:String;
+	title:string;
+	message:string;
+	cancelButtonLabel:string;
+	laterButtonLabel:string;
+	rateButtonLabel:string;
 }
 
 declare class Locales {
 	addLocale(localeObject:Locale):Locale;
-	getLocale(language:String, applicationTitle?:String):Locale;
-	getLocalesNames():Array<String>;
+	getLocale(language:string, applicationTitle?:string):Locale;
+	getLocalesNames():Array<string>;
 }
 
 declare class Locale {
@@ -70,10 +70,10 @@ declare class Locale {
 }
 
 declare class LocaleOptions {
-	language:String
-	title:String;
-	message:String;
-	cancelButtonLabel:String;
-	laterButtonLabel:String;
-	rateButtonLabel:String;
+	language:string
+	title:string;
+	message:string;
+	cancelButtonLabel:string;
+	laterButtonLabel:string;
+	rateButtonLabel:string;
 }
