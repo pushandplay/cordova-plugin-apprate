c7bf75eec1fe5a7aeaf82d130285f3fe9005144c
Alex Ryltsov <ryltsov.alex@gmail.com>
2024-06-26 14:53:46 +0300

    chore: Migration from the Play Core Java
    Changes:
    - migrate from the google play SDK to avoid warning from google play when the app is targeting SDK 34+ (targetSdkVersion). For more details please see https://github.com/pushandplay/cordova-plugin-apprate/issues/332

--- src/android/AppRate.java
+++ src/android/AppRate.java
@@ -15,7 +15,7 @@ import android.content.pm.ApplicationInfo;
 import com.google.android.play.core.review.ReviewInfo;
 import com.google.android.play.core.review.ReviewManager;
 import com.google.android.play.core.review.ReviewManagerFactory;
-import com.google.android.play.core.tasks.Task;
+import com.google.android.gms.tasks.Task;
 
 public class AppRate extends CordovaPlugin {
   @Override
